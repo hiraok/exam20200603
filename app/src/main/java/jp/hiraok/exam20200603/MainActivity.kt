b0d77629d2f660bd423371b2f051f4c1ffa3b055
hiraok <khirao.dev.work@gmail.com>
2020-06-14 00:37:05 +0900

    navigationView追加

--- app/src/main/java/jp/hiraok/exam20200603/MainActivity.kt
+++ app/src/main/java/jp/hiraok/exam20200603/MainActivity.kt
@@ -2,13 +2,25 @@ package jp.hiraok.exam20200603
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        NavigationView.OnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.contributor -> {
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.myaccount -> {
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 
 }
