2ab30e03b9c6894ff3d4528f5086feee30c3f411
hiraok <khirao.dev.work@gmail.com>
2020-06-18 02:54:29 +0900

    add circle image transform

---
+++ app/src/main/java/jp/hiraok/exam20200603/CircleTransform.kt
@@ -0,0 +1,49 @@
+package jp.hiraok.exam20200603
+
+import android.graphics.*
+import com.squareup.picasso.Transformation
+import java.lang.Integer.min
+
+class CircleTransform : Transformation {
+
+    companion object {
+        const val KEY = "circleView"
+    }
+
+    override fun key(): String = KEY
+
+    override fun transform(source: Bitmap?): Bitmap {
+        source?.let {
+            val size = min(it.width, it.height)
+
+            val x = (it.width - size) / 2
+            val y = (it.height - size) / 2
+
+            val squaredBitmap = Bitmap.createBitmap(it, x, y, size, size)
+            if (squaredBitmap != it) {
+                it.recycle()
+            }
+
+            val bitmap = Bitmap.createBitmap(size, size, it.config)
+
+            val canvas = Canvas(bitmap)
+            val paint = Paint()
+            val shader = BitmapShader(
+                squaredBitmap,
+                Shader.TileMode.CLAMP, Shader.TileMode.CLAMP
+            )
+
+            paint.shader = shader
+            paint.isAntiAlias = true
+
+            val r = size / 2f
+            canvas.drawCircle(r, r, r, paint)
+
+            squaredBitmap.recycle()
+            return bitmap
+
+        } ?: throw IllegalStateException("bitmap source is null")
+    }
+
+
+}
